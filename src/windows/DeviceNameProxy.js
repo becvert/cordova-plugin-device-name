44edaaf3a938d5146e9216145893c25c10571820
Philipp Kursawe <phil.kursawe@gmail.com>
2016-11-22 01:08:33 +0100

    Fix: Whitespace

--- src/windows/DeviceNameProxy.js
+++ src/windows/DeviceNameProxy.js
@@ -2,18 +2,18 @@
 module.exports = {
 
     get: function getDeviceName(success, fail) {
-		if (!Windows
+        if (!Windows
             || !Windows.Security
             || !Windows.Security.ExchangeActiveSyncProvisioning
             || !Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation) {
-			return fail(new Error("WinRT API Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation not found"));
-		}
+            return fail(new Error("WinRT API Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation not found"));
+        }
 
-		var info = new Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
-		return success({
+        var info = new Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
+        return success({
             name: info.friendlyName
         });
-	}
+    }
 };
 
 require("cordova/exec/proxy").add("DeviceName", module.exports);
