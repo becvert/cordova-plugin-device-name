109dc769d37abef01ee11bab98435e72924d4d60
becvert <sylvain.brejeon@gmail.com>
2017-09-07 11:20:11 +0200

    [Android] removed BLUETOOTH permission using Global and Secure system settings instead.

--- src/android/net/becvert/cordova/DeviceName.java
+++ src/android/net/becvert/cordova/DeviceName.java
@@ -4,13 +4,22 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 
-import android.bluetooth.BluetoothAdapter;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
 
 public class DeviceName extends CordovaPlugin {
 
+    private static final String TAG = "DeviceName";
+
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         if ("get".equals(action)) {
-            callbackContext.success(this.getName());
+            String name = this.getName();
+            if (name == null) {
+                callbackContext.error("could not retrieve the name of the device");
+            } else {
+                callbackContext.success(name);
+            }
         } else {
             return false;
         }
@@ -18,12 +27,16 @@ public class DeviceName extends CordovaPlugin {
     }
 
     public String getName() {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            return null;
-        } else {
-            return mBluetoothAdapter.getName();
+        String name = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Log.d(TAG, "device_name");
+            name = Settings.Global.getString(cordova.getActivity().getContentResolver(), "device_name");
         }
+        if (name == null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            Log.d(TAG, "bluetooth_name");
+            name = Settings.Secure.getString(cordova.getActivity().getContentResolver(), "bluetooth_name");
+        }
+        return name;
     }
 
 }
