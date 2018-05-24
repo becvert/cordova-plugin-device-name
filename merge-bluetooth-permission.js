56245dadbf449d04d6d05a9f013e58a665f83e49
becvert <sylvain.brejeon@gmail.com>
2018-05-24 08:51:04 +0200

    fixing hook to work with cordova 7

--- merge-bluetooth-permission.js
+++ merge-bluetooth-permission.js
@@ -8,27 +8,35 @@ module.exports = function(context) {
 
     var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
 
-    var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
+    var preCordova7ManifestFile = path.join(platformRoot, 'AndroidManifest.xml');
 
-    if (fs.existsSync(manifestFile)) {
-        fs.readFile(manifestFile, 'utf8', function (err,data) {
-            if (err) {
-                throw new Error('Unable to read AndroidManifest.xml: ' + err);
+    var cordova7ManifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
+
+    var manifestEdit = function(file) {
+      fs.readFile(file, 'utf8', function (err,data) {
+        if (err) {
+            throw new Error('Unable to read AndroidManifest.xml: ' + err);
+        }
+        var permissions = data.match(/<uses-permission(.*?)\/>/g);
+        var bluetoothPermissions = permissions.filter(permission => permission.indexOf('android:name="android.permission.BLUETOOTH"') > -1);
+        if (bluetoothPermissions.length > 1) {
+            var permissionWithoutMaxSDK = bluetoothPermissions.find(permission => permission.indexOf('android:maxSdkVersion') == -1);
+            if (permissionWithoutMaxSDK) {
+                var result = data.replace('<uses-permission android:maxSdkVersion="18" android:name="android.permission.BLUETOOTH" />', '');
+                fs.writeFile(file, result, 'utf8', function (err) {
+                    if (err) {
+                        throw new Error('Unable to write into AndroidManifest.xml: ' + err);
+                    }
+                });
             }
-            var permissions = data.match(/<uses-permission(.*?)\/>/g);
-            var bluetoothPermissions = permissions.filter(permission => permission.toUpperCase().indexOf('ANDROID.PERMISSION.BLUETOOTH') > -1);
-            if (bluetoothPermissions.length > 1) {
-                var permissionWithoutMaxSDK = bluetoothPermissions.find(permission => permission.indexOf('android:maxSdkVersion') == -1);
-                if (permissionWithoutMaxSDK) {
-                    var result = data.replace('<uses-permission android:maxSdkVersion="18" android:name="android.permission.BLUETOOTH" />', '');
-                    fs.writeFile(manifestFile, result, 'utf8', function (err) {
-                        if (err) {
-                            throw new Error('Unable to write into AndroidManifest.xml: ' + err);
-                        }
-                    });
-                }
-            }
-        });
+        }
+      });
+    }
+
+    if (fs.existsSync(preCordova7ManifestFile)) {
+      manifestEdit(preCordova7ManifestFile)
+    } else if (fs.existsSync(cordova7ManifestFile)) {
+      manifestEdit(cordova7ManifestFile)
     }
 
 };
